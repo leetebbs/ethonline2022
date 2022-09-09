c347dc2014f71967d682fd0756e594f38772518f
Tebbo <leetebbs@ymail.com>
2022-09-09 11:50:52 +0100

    add tipping and navbar

--- tribe/src/components/Navbar.js
+++ tribe/src/components/Navbar.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import image from '../images/home.png'
 import send from '../images/send.png'
+import add from'../images/add.png'
+import compass from '../images/compass.png'
+import heart from '../images/heart.png'
+import search from '../images/search.png'
+
+
 function Navbar() {
   return (
     <div className="nav">
         <h1 className="logo">Tribe</h1>
+        <div className="search">
+            <img className="search-icon" src={search}/>
+            search
+        </div>
+        <div className="icons">
         <img src={image}/>
         <img className="send" src={send}/>
+        <img className="add" src={add}/>
+        <img className="add" src={compass}/>
+        <img className="add" src={heart}/>
+        </div>
     </div>
     
   )
